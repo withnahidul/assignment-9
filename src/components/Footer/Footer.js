38b7cbf480e036f2b59aed68e6abf173c86ea792
withnahidul <nahidulislam6800@gmail.com>
2022-04-04 23:31:10 +0600

    footer-add

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
-    return (
-        <footer className='py-1 mt-5 static bottom-0 h-10 w-full bg-black text-white'><p className='text-lg'>All Rights Reserved. Rashadul Alam Rimon | 2022</p></footer>
-    );
+  return (
+    <footer className="py-1 mt-5 static bottom-0 h-10 w-full bg-black text-white">
+      <p className="text-lg">All Rights Reserved. | Md. Nahidul Islam | 2022</p>
+    </footer>
+  );
 };
 
 export default Footer;
